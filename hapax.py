54e6b7ac7d6cf035fb2fc648d01f1dde120a22d6
Miquel Llobet <mllobet.cm@gmail.com>
2022-04-14 18:51:59 +0200

    V1 hapax finder, debugging still needed

--- hapax.py
+++ hapax.py
@@ -8,15 +8,36 @@ from os.path import isfile, join
 
 from collections import defaultdict
 
-## *** START OF THIS PROJECT GUTENBERG EBOOK APOCOLOCYNTOSIS ***
-
+# Removes all unwanted characters
 def clean_word(word):
-    # remove non-alphanumeric characters
+    # remove all caps words
+    no_caps = ''.join(filter(lambda c: not c.isupper(), word))
+    if no_caps == '':
+        return ''
+
+    # remove URLs
+    # www
+    if len(word) >= 3:
+        if word[:3] == "www":
+            return ''
+    # http
+    if len(word) >= 4:
+        if word[:4] == "http":
+            return ''
+
     clean = ''.join(filter(str.isalnum, word))
     clean = ''.join(filter(lambda c: c.isalpha(), clean))
 
+    # transform to lowercase
+    clean = clean.lower()
+
+
+    # print("BEFORE {} AFTER {}".format(word, clean))
+
     return clean
 
+# Finds happaxes given a text file
+# Returns a dictionary containing all hapaxes
 def find_hapax(file):
     d = defaultdict(lambda: 0)
     
@@ -32,7 +53,7 @@ def find_hapax(file):
             d[clean_w] += 1
 
     hapaxes = dict(filter(lambda elem: elem[1] == 1, d.items()))
-    print(hapaxes)
+    # print(hapaxes)
     return hapaxes
 
 def run(files, directory):
@@ -42,10 +63,13 @@ def run(files, directory):
         directory_files = [f for f in listdir(directory) if isfile(join(directory, f))]
     all_files = files + directory_files 
 
-    # run hapaxer for each file
+    # run hapax finder for each file
     for file in files:
         with open(file, 'r') as f:
-           print(len(find_hapax(f))) 
+           hapax_dict = find_hapax(f)
+           print("Hapax count: {}".format(len(hapax_dict)))
+           for k, v in hapax_dict.items():
+               print(k)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Finds words that appear only once in a given text (hapaxes)")
@@ -55,5 +79,3 @@ if __name__ == "__main__":
     args = parser.parse_args(sys.argv[1:])
     print (args)
     run(args.file, args.directory)
-
-    
