4f3466ce1f68f2ce276718667a97f0463515a821
Miquel Llobet <mllobet.cm@gmail.com>
2022-04-14 22:23:51 +0200

    Updated hapax to output files and ran once

--- hapax.py
+++ hapax.py
@@ -10,10 +10,6 @@ from collections import defaultdict
 
 # Removes all unwanted characters
 def clean_word(word):
-    # remove all caps words
-    no_caps = ''.join(filter(lambda c: not c.isupper(), word))
-    if no_caps == '':
-        return ''
 
     # remove URLs
     # www
@@ -25,14 +21,19 @@ def clean_word(word):
         if word[:4] == "http":
             return ''
 
-    clean = ''.join(filter(str.isalnum, word))
-    clean = ''.join(filter(lambda c: c.isalpha(), clean))
+    # keep only letters, no numbers/punctuation
+    clean = ''.join(filter(lambda c: c.isalpha(), word))
+
+    # remove all caps words
+    no_caps = ''.join(filter(lambda c: not c.isupper(), clean))
+    if no_caps == '':
+        return ''
 
     # transform to lowercase
     clean = clean.lower()
 
 
-    # print("BEFORE {} AFTER {}".format(word, clean))
+    #print("---\nBEFORE {} AFTER {}\n".format(word, clean), file=sys.stderr)
 
     return clean
 
@@ -56,26 +57,35 @@ def find_hapax(file):
     # print(hapaxes)
     return hapaxes
 
-def run(files, directory):
+def run(files, input_directory, output_directory):
     # flatten files
     directory_files = []
-    if directory:
-        directory_files = [f for f in listdir(directory) if isfile(join(directory, f))]
+    if input_directory:
+        directory_files = [input_directory + '/' + f for f in listdir(input_directory) if isfile(join(input_directory, f))]
     all_files = files + directory_files 
 
     # run hapax finder for each file
-    for file in files:
+    for file in all_files:
+        print(file)
         with open(file, 'r') as f:
-           hapax_dict = find_hapax(f)
-           print("Hapax count: {}".format(len(hapax_dict)))
-           for k, v in hapax_dict.items():
-               print(k)
+            # rename outfile
+            outfile = file.replace("txt", "hx")
+            i = outfile.find('/')
+            if i >= 0:
+                outfile = output_directory + '/' + outfile[i+1:]
+
+            with open(outfile, 'w') as w:
+                hapax_dict = find_hapax(f)
+                print("Hapax count: {}".format(len(hapax_dict)))
+                for k, v in hapax_dict.items():
+                    w.write(k)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Finds words that appear only once in a given text (hapaxes)")
     parser.add_argument('-f', '--file', help="the file(s) to check for hapaxes on", nargs='+', default=[])
-    parser.add_argument('-d', '--directory', help="the directory of text files to check for hapaxes on") 
+    parser.add_argument('-d', '--directory', help="the directory of text files to check for hapaxes on", default="") 
+    parser.add_argument('-o', '--output-dir', help="the output directory to store hapax files", default="hapaxes")
 
     args = parser.parse_args(sys.argv[1:])
     print (args)
-    run(args.file, args.directory)
+    run(args.file, args.directory, args.output_dir)
